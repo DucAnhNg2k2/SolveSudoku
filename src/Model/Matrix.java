92fe37bf11516c49204a7a1db6e239f41c287727
_DucAnh_ <ngdanh2k2@gmail.com>
2022-03-14 09:31:15 +0700

    sudoku

--- src/Model/Matrix.java
+++ src/Model/Matrix.java
@@ -8,15 +8,15 @@ import java.util.Scanner;
  */
 public class Matrix {
     /* demo 
-0 0 0 0 1 8 0 6 0 
-0 0 0 3 0 0 0 5 0 
-0 6 0 0 0 0 0 4 0
-0 0 0 0 5 0 0 0 8
+0 0 0 0 0 0 0 2 0 
+0 0 6 8 0 0 3 0 7 
+0 1 0 0 9 0 0 0 0
+0 0 5 0 0 0 0 4 0
+9 0 0 0 8 0 5 0 2
+0 0 0 2 0 0 0 6 0
 6 0 0 0 0 3 0 0 0
-0 7 9 0 4 0 2 0 0
-0 4 0 0 0 0 3 0 0
-5 0 0 0 0 2 0 1 6
-0 2 3 6 0 0 0 0 0
+0 0 3 7 0 0 8 0 5
+0 0 0 0 0 0 4 0 0
     */
     private Scanner sc = new Scanner(System.in);
     public int matrix[][] = new int[10][10];
